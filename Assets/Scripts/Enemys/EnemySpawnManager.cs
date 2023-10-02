2db98c5fcfd4e6aea6f8d073ac29301f79d0c62c
Rádli Dorián <dorian.radli.work@gmail.com>
2023-10-02 14:26:37 +0200

    LastComit

--- Assets/Scripts/Enemys/EnemySpawnManager.cs
+++ Assets/Scripts/Enemys/EnemySpawnManager.cs
@@ -1,27 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
     public GameObject[] enemyCharachters;
     public GameObject[] spawnPoints;
+    
     private int actualSpawnPoint;
+    int current;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("EnemySpawn", 30f, 15f);
+        
     }
 
     void Update()
     {
+       
         actualSpawnPoint = Random.Range(0, spawnPoints.Length);
     }
     void EnemySpawn()
     {
         
-        Instantiate(enemyCharachters[Random.Range(0, enemyCharachters.Length)], spawnPoints[actualSpawnPoint].transform.position + new Vector3(0, 0, 2), spawnPoints[actualSpawnPoint].transform.rotation);
+        Instantiate(enemyCharachters[Random.Range(0, enemyCharachters.Length)], spawnPoints[actualSpawnPoint].transform.position, spawnPoints[actualSpawnPoint].transform.rotation);
     }
+
+
 
 
 
